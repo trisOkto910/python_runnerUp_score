a3d223d253cf39e42a62d35d10ee957758e8193d
Tris <60920888+trisOkto910@users.noreply.github.com>
2021-11-21 23:29:02 +0700

    Update main.py
    update get runner up score

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ n = int(input('enter the size of list:'))
 arr = list(map(int, input('Enter entries with space:').split()))
 
 arr.sort()
-k = arr[0]
-for i in arr:
-    if i > k:
-        k = i
-for l in range(arr.count(k)):
-    arr.remove(k)
-print(arr[-1])
+
+print(f'hasil sorting array {arr}')
+
+print(f'result runner up score {arr[-2]}')
